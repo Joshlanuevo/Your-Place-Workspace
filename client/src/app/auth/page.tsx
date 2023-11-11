c1b9cf4bbd899c7c2906451b56e7ce305fe7da99
Joshlanuevo <lanuevo68@gmail.com>
2023-11-11 15:29:31 +0800

    auth page added.

---
+++ client/src/app/auth/page.tsx
@@ -0,0 +1,60 @@
+"use client"
+import Link from "next/link";
+import Card from "../shared/components/UIElements/Card";
+import Input from "../shared/components/FormElements/Input";
+import Button from "../shared/components/FormElements/Button";
+import { VALIDATOR_REQUIRE } from "../shared/util/validator";
+
+const AuthPage = () => {
+    return (
+        <div className="flex justify-center items-center h-screen mt-10">
+            <div className="w-80 sm:w-96">
+                <h1 className="text-2xl text-center text-white font-semibold mb-10">
+                    Sign in to 
+                    <span className="text-2xl text-white bg-[#5865F2] mx-2 py-2 px-4 rounded-lg font-bold">
+                        YourPlace
+                    </span>
+                </h1>
+                <Card className="bg-gray-300 p-4">
+                    <form>
+                        <Input
+                            id="name"
+                            element="input"
+                            label="Name"
+                            validators={[VALIDATOR_REQUIRE()]}
+                            onInput={() => console.log('wazzup')}
+                        />
+                        <Input
+                            id="email"
+                            element="input"
+                            type="email"
+                            label="Email"
+                            validators={[VALIDATOR_REQUIRE()]}
+                            onInput={() => console.log('wazzup')}
+                        />
+                        <Input
+                            id="password"
+                            element="input"
+                            type="password"
+                            label="Password"
+                            validators={[VALIDATOR_REQUIRE()]}
+                            onInput={() => console.log('wazzup')}
+                        />
+                        <Button wide className="mt-4 bg-blue-500 hover:bg-blue-700 text-white">
+                            SIGN IN
+                        </Button>
+                    </form>
+                </Card>
+                <div className="mt-6">
+                    <Card className="bg-gray-300 p-4">
+                        <h3 className="text-sm text-center text-black font-semibold">
+                            New to YourPlace? <Link href="/" className="text-blue-500">Create an account</Link>
+                        </h3>
+                    </Card>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default AuthPage;
