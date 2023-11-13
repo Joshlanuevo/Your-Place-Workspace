b78d7c72287341b695116640701a14c918b33181
Joshlanuevo <lanuevo68@gmail.com>
2023-11-13 16:48:57 +0800

    finishing frontend

--- client/src/app/shared/components/Navigations/NavLinks.tsx
+++ client/src/app/shared/components/Navigations/NavLinks.tsx
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
+import { AuthContext } from '../../context/auth-context';
 
 const NavLinks: React.FC = () => {
+  const auth = useContext(AuthContext);
   const pathname = usePathname();
 
   const isActive = (href: string) => pathname === href;
@@ -10,21 +12,21 @@ const NavLinks: React.FC = () => {
   const navLinks = [
     { href: '/', name: 'All Users' },
     { href: '/places/u1', name: 'My Places' },
-    // { href: '/places/new', name: 'Add Place' },
-    { href: '/auth', name: 'Sign in' },
+    { href: '/auth', name: auth.isLoggedIn ? 'Logout' : 'Sign in' },
   ];
 
   return (
-    <ul className="nav-links list-none m-0 p-0 w-full h-full flex flex-col 
-    sm:flex-row justify-center items-center uppercase text-white">
+    <ul className="nav-links list-none m-0 p-0 w-full h-full flex flex-col sm:flex-row justify-center items-center uppercase text-white">
       {navLinks.map((link) => (
         <li
           className={`mb-2 sm:mb-0 sm:mr-2 ${isActive(link.href) ? 'text-[#5865F2]' : ''}`}
           key={link.name}
         >
-          <Link href={link.href}>
-            {link.name}
-          </Link>
+          {link.name === 'Logout' ? (
+            <button onClick={auth.logout}>{link.name}</button>
+          ) : (
+            <Link href={link.href}>{link.name}</Link>
+          )}
         </li>
       ))}
     </ul>
