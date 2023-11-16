ae8d374447122c9f078b5473c5cbd97f6e9ff672
Joshlanuevo <lanuevo68@gmail.com>
2023-11-16 16:22:48 +0800

    delete button fixed.

--- client/src/app/shared/components/FormElements/Button.tsx
+++ client/src/app/shared/components/FormElements/Button.tsx
@@ -12,7 +12,7 @@ const Button = (props: any) => {
             ${props.primary ? 'text-white bg-[#5865F2] hover:bg-blue-500': ''}
             ${props.inverse ? 'text-primary border-primary bg-transparent' : 'text-white border-primary bg-primary'}
             ${props.secondary ? 'text-white bg-[#a7a7a7] hover:bg-[#b4b4b4]' : 'text-white border-primary bg-primary'}
-            ${props.danger ? `border-${props.danger} bg-${props.danger} hover:bg-dangerHover` : ''}
+            ${props.danger ? `bg-[#FF4C4C] hover:bg-red-600` : ''}
             ${props.disabled ? 'bg-gray-400 text-gray-700 border-gray-400 cursor-not-allowed' : ''}
           `}
         >
@@ -32,7 +32,7 @@ const Button = (props: any) => {
         ${props.inverse ? 'text-primary border-primary bg-transparent' : 'text-white border-primary bg-primary'}
         ${props.secondary ? 'text-white bg-[#a7a7a7] hover:bg-[#b4b4b4]' : 'text-white border-primary bg-primary'}
         ${props.circle ? 'text-white bg-[#5865F2] hover:bg-blue-500 rounded-full': ''}
-        ${props.danger ? `bg-[#FF4C4C] hover:bg-[#f34343]` : ''}
+        ${props.danger ? `bg-[#FF4C4C] hover:bg-red-600` : ''}
         ${props.wide ? 'border-none w-full' : ''}
         ${props.disabled ? 'bg-gray-400 text-gray-700 border-gray-400 cursor-not-allowed' : 'text-white bg-[#5865F2] hover:bg-blue-500'}
       `}
