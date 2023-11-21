397c01c6fddfac67d10111894ef8e8970aee3290
Joshlanuevo <lanuevo68@gmail.com>
2023-11-21 11:28:18 +0800

    loading component added.

---
+++ client/src/app/shared/components/UIElements/LoadingSpinner.tsx
@@ -0,0 +1,10 @@
+const LoadingSpinner = (props: any) => {
+    return (
+      <div className={`${props.asOverlay && 'loading-spinner__overlay'} h-full w-full absolute top-0 left-0 bg-white bg-opacity-90 flex justify-center items-center`}>
+        <div className="lds-dual-ring"></div>
+      </div>
+    );
+  };
+  
+  export default LoadingSpinner;
+  
