8a0db19b2d0dcc3c9ed42979915cad629e086b4a
Joshlanuevo <lanuevo68@gmail.com>
2023-12-08 11:26:17 +0800

    fs implemented.

--- api/app.js
+++ api/app.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
@@ -9,6 +10,8 @@ const usersRoutes = require('./routes/users-routes');
 const placesRoutes = require('./routes/places-routes');
 
 app.use(bodyParser.json());
+
+app.use('/uploads/images', express.static(path.join('uploads', 'images')));
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
@@ -30,6 +33,11 @@ app.use((req, res, next) => {
 });
 
 app.use((error, req, res, next) => {
+  if (req.file) {
+    fs.unlink(req.file.path, err => {
+      console.log(err);
+    })
+  }
   if (res.headerSent) {
     return next(error);
   }
